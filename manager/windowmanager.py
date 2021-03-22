ca90d107926f0f6aa30f02c944b625e39674f0f2
blakeyzhang <blakeyzhang@tencent.com>
2021-03-22 21:25:39 +0800

    修改android 10设备获取qq控件异常

--- manager/windowmanager.py
+++ manager/windowmanager.py
@@ -171,13 +171,19 @@ class WindowManager(BaseManager):
     def _get_windows_data(self):
         '''获取windows数据并解析
         '''
+        window_name = None
         result = self._device.adb.run_shell_cmd('dumpsys window windows')
         result = result.replace('\r', '')
-        # print result
+        current_activity = self._device.adb.run_shell_cmd("dumpsys activity a . | grep -E 'mResumedActivity' | cut -d ' ' -f 8")
+        if '/' in current_activity:
+            package_name, activity_name = current_activity.split('/')
+            window_name = package_name + '/' + package_name + activity_name
+        # print  result
         windows = []
         window = None
         p1 = re.compile(r'^  Window #(\d+) Window{(\w{6,9}) (.*)}:$')
-        p2 = re.compile(r'Window{(\w{6,9}) (u0 ){0,1}(\S+).*}')
+        # p2 = re.compile(r'Window{(\w{6,9}) (u0 ){0,1}(\S+).*}')
+        p2 =re.compile(r'Window{(\w+) (u0 ){0,1}(\S+).*}')
         p3 = re.compile(r'mShownFrame=\[([-\d\.]+),([-\d\.]+)\]\[([-\d\.]+),([-\d\.]+)\]')
         for line in result.split('\n')[1:]:
             # print repr(line)
@@ -188,16 +194,19 @@ class WindowManager(BaseManager):
                     items = title.split(' ')
                     if 'u0' == items[0]: title = items[1]
                     else: title = items[0]
+                if title == window_name:
+                    self._current_window = Window(self, int(ret.group(1)), ret.group(2), title)
                 window = Window(self, int(ret.group(1)), ret.group(2), title)  # 此逻辑可能有bug
                 windows.append(window)
-            elif 'mCurrentFocus' in line:
-                ret = p2.search(line)
-                if ret:
-                    title = ret.group(3)
-                    self._current_window = Window(self, 0, ret.group(1), title)
-                else:
-                    Log.w('WindowManager', line)
-                    self._current_window = None
+            elif 'mObscuringWindow' in line:
+                if 'mCurrentFocus' in line:
+                    ret = p2.search(line)
+                    if ret:
+                        title = ret.group(3)
+                        self._current_window = Window(self, 0, ret.group(1), title)
+                    else:
+                        Log.w("WindowManager", line)
+                        self._current_window = None
             elif 'mInputMethodTarget' in line:
                 ret = p2.search(line)
                 self._current_input_target = Window(self, 0, ret.group(1), ret.group(2) if ret.group(2) and len(ret.group(2)) > 5 else ret.group(3))
